b7795780098358421057181751a6e7849c6ea383
PAWEŁ IWANOWSKI <pawel.iwanowski@itwl.pl>
2020-10-07 08:00:18 +0200

    Add project files.

---
+++ AirContent/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.airText = new System.Windows.Forms.TextBox();
+            this.phaseText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.airPhaseText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.airPhaseAll = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "air";
+            // 
+            // airText
+            // 
+            this.airText.Location = new System.Drawing.Point(78, 40);
+            this.airText.Multiline = true;
+            this.airText.Name = "airText";
+            this.airText.Size = new System.Drawing.Size(355, 101);
+            this.airText.TabIndex = 2;
+            // 
+            // phaseText
+            // 
+            this.phaseText.Location = new System.Drawing.Point(78, 147);
+            this.phaseText.Multiline = true;
+            this.phaseText.Name = "phaseText";
+            this.phaseText.Size = new System.Drawing.Size(355, 100);
+            this.phaseText.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "phase";
+            // 
+            // airPhaseText
+            // 
+            this.airPhaseText.Location = new System.Drawing.Point(78, 253);
+            this.airPhaseText.Multiline = true;
+            this.airPhaseText.Name = "airPhaseText";
+            this.airPhaseText.Size = new System.Drawing.Size(355, 99);
+            this.airPhaseText.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "air/phase";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "air/phase for all";
+            // 
+            // airPhaseAll
+            // 
+            this.airPhaseAll.Location = new System.Drawing.Point(110, 366);
+            this.airPhaseAll.Name = "airPhaseAll";
+            this.airPhaseAll.Size = new System.Drawing.Size(100, 20);
+            this.airPhaseAll.TabIndex = 8;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 396);
+            this.Controls.Add(this.airPhaseAll);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.airPhaseText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.phaseText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.airText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "mainWindow";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox airText;
+        private System.Windows.Forms.TextBox phaseText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox airPhaseText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox airPhaseAll;
+    }
+}
+
---
+++ AirContent/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class mainWindow : Form
+    {
+        Bitmap image;
+        double air = 0, phase = 0, airSum = 0, phaseSum = 0;
+        double airPhaseRatio = 1, airPhaseRatioAverage = 1;
+        byte r, g, b;
+        Color pixel;
+
+        string airString, phaseString, airPhaseRatioString;
+
+        public mainWindow()
+        {
+            InitializeComponent();
+            this.Text = "Air Content v.1.3";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            air = 0;
+            phase = 0;
+            airSum = 0;
+            phaseSum = 0;
+            airPhaseRatio = 1;
+            airPhaseRatioAverage = 1;
+            airString = "";
+            phaseString = "";
+            airPhaseRatioString = "";
+            airText.Text = "";
+            phaseText.Text = "";
+            airPhaseText.Text = "";
+            airPhaseAll.Text = "";
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.ShowDialog();
+            
+            if (dlg.FileNames != null)
+            {
+                foreach (string path in dlg.FileNames)
+                {
+                    image = new Bitmap(path);
+
+                    //for (int i = 0; i < image.Height; i++)
+                    //{
+                        int x = -1, y = image.Width;
+                        int i = image.Height / 2;
+                        do
+                        {
+                            x++;
+                            pixel = image.GetPixel(x, i);
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+                        } while (r == 255 && g == 255 && b == 0);
+                        
+                        do
+                        {
+                            y--;
+                            pixel = image.GetPixel(y, i);
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+
+                        } while (r == 255 && g == 255 && b == 0);
+
+                        if (x > y)
+                        {
+                            y = x + 1;
+                        }
+
+                        for (int j = x; j < y; j++)
+                        {
+                            pixel = image.GetPixel(j, i);
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+
+                            if (r == 255 && g == 255 && b == 0)
+                            {
+                                air++;
+                            }
+                            else
+                            {
+                                phase++;
+                            }
+                        }
+                    //}
+
+                    if (phase != 0)
+                    {
+                        airPhaseRatio = air / phase * 100;
+                        airPhaseRatio = Math.Round(airPhaseRatio, 2);
+                    }
+
+                    airString += air.ToString() + ", ";
+                    phaseString += phase.ToString() + ", ";
+                    airPhaseRatioString += airPhaseRatio.ToString() + " %, ";
+
+                    if (airPhaseRatio > 1)
+                    {
+                        airSum += air;
+                        phaseSum += phase;
+                    }
+
+                    air = 0;
+                    phase = 0;
+                    airPhaseRatio = 1;
+                }
+
+                if (phaseSum != 0)
+                {
+                    airPhaseRatioAverage = airSum / phaseSum * 100;
+                    airPhaseRatioAverage = Math.Round(airPhaseRatioAverage, 2);
+                }
+
+                airText.Text = airString;
+                phaseText.Text = phaseString;
+                airPhaseText.Text = airPhaseRatioString;
+                airPhaseAll.Text = airPhaseRatioAverage.ToString() + " %";
+            }
+        }
+    }
+}
